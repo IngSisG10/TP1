9abb807f1f1ddc19710d0c6cb9e03e03448da268
FranManfredi <FManfredi@mail.austral.edu.ar>
2025-08-07 14:43:50 -0300

    added missing tokens

---
+++ src/token/FunctionToken.kt
@@ -0,0 +1,19 @@
+package token
+
+import token.abs.TokenInterface
+
+enum class Function {
+    PRINTLN
+}
+
+class FunctionToken(
+    override val value: Function,
+    override val row: Int,
+    override val position: Int
+) : TokenInterface {
+    override val name: String = "function";
+    override fun instruction(): String {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ src/token/ParenthesisToken.kt
@@ -0,0 +1,20 @@
+package token
+
+import token.abs.TokenInterface
+
+enum class Parenthesis {
+    OPEN,
+    CLOSE
+}
+
+class ParenthesisToken(
+    override val value: Parenthesis,
+    override val row: Int,
+    override val position: Int
+) : TokenInterface {
+    override val name: String= "parenthesis"
+    override fun instruction(): String {
+        TODO("Not yet implemented")
+    }
+
+}
